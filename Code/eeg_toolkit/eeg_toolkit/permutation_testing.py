e0252d39e6e80326b6ded737feae561386d9a907
George Kenefati <georgekenefati98@gmail.com>
2024-05-01 13:51:44 -0400

    feat: completed parallelization of stc perm and fc code w 500 reps; changed hist bin size

--- Code/eeg_toolkit/eeg_toolkit/permutation_testing.py
+++ Code/eeg_toolkit/eeg_toolkit/permutation_testing.py
@@ -26,3 +26,26 @@ def create_surrogate_data(original_data):
 
     # Return the real part of the surrogate data
     return np.real(surrogate_data)
+
+
+# Create AAFT surrogate
+def create_aaft_surrogate(original_data):  
+    # Step 1: Generate white noise with the same variance as the original data  
+    white_noise = np.random.normal(0, np.std(original_data), len(original_data))  
+  
+    # Step 2: Sort the white noise in the order of the sorted original data  
+    surrogate_data = np.sort(white_noise)[np.argsort(np.argsort(original_data))]  
+  
+    # Step 3: Do a Fourier transform on the surrogate data  
+    fft_surrogate = np.fft.fft(surrogate_data)  
+  
+    # Step 4: Generate phase randomized surrogate data  
+    phase_randomized_surrogate = np.abs(fft_surrogate) * np.exp(1j * 2 * np.pi * np.random.random(len(original_data)))  
+  
+    # Step 5: Do an inverse Fourier transform on the phase randomized surrogate  
+    inverse_fft = np.fft.ifft(phase_randomized_surrogate)  
+  
+    # Step 6: Sort the inverse FFT in the order of the sorted surrogate data from step 2  
+    final_surrogate = np.sort(np.real(inverse_fft))[np.argsort(np.argsort(surrogate_data))]  
+  
+    return final_surrogate  
